82678aaf56ef7d1b962b73f204cb8c7880271258
Kaveesha <kaveeshewaduge111@gmail.com>
2024-12-31 17:48:40 +0530

    header section in admin page

--- healthcare-next/components/PasskeyModal.tsx
+++ healthcare-next/components/PasskeyModal.tsx
@@ -18,7 +18,7 @@ import {
     InputOTPSeparator,
     InputOTPSlot,
 } from "@/components/ui/input-otp"
-import { encryptKey } from "@/lib/utils";
+import { decryptKey, encryptKey } from "@/lib/utils";
   
 import Image from "next/image";
 import { usePathname, useRouter } from "next/navigation";
@@ -34,9 +34,11 @@ const PasskeyModal = () => {
 
   const encryptedKey = typeof window !== 'undefined' ? window.localStorage.getItem('accessKey') : null;
 
+  const accessKey = encryptedKey && decryptKey(encryptedKey);
+
   useEffect(() => {
     if(path) {
-        if(passkey === process.env.NEXT_PUBLIC_ADMIN_PASSKEY) {
+        if(accessKey === process.env.NEXT_PUBLIC_ADMIN_PASSKEY) {
             setOpen(false);
             router.push('/admin');
         } else {
